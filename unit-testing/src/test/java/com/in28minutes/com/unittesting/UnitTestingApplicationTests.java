ab959994850060aa8cc6b036d550e75e6d663a17
Rahul Ratra <rahul.ratra51@gmail.com>
2020-04-08 18:51:25 -0400

    <Junit Test>

---
+++ unit-testing/src/test/java/com/in28minutes/com/unittesting/UnitTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.com.unittesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
