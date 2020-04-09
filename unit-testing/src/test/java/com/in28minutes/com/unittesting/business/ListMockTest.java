46829fc7cec19ca37c5467b6ee75727ec2ee90b9
Rahul Ratra <rahul.ratra51@gmail.com>
2020-04-08 22:49:57 -0400

    <test>

--- unit-testing/src/test/java/com/in28minutes/com/unittesting/business/ListMockTest.java
+++ unit-testing/src/test/java/com/in28minutes/com/unittesting/business/ListMockTest.java
@@ -1,6 +1,11 @@
 package com.in28minutes.com.unittesting.business;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.atLeast;
+import static org.mockito.Mockito.atMost;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.List;
@@ -10,7 +15,7 @@ import org.mockito.Mockito;
 import static org.mockito.ArgumentMatchers.anyInt;
 
 public class ListMockTest {
-	List mock = Mockito.mock(List.class);
+	List<String> mock = Mockito.mock(List.class);
 	@Test
 	public void basic_test()
 	{
@@ -40,5 +45,18 @@ public class ListMockTest {
 		when(mock.get(anyInt())).thenReturn("in28Minutes");
 		assertEquals("in28Minutes", mock.get(0));
 		assertEquals("in28Minutes", mock.get(1));
+	}
+	
+	@Test
+	public void verificationBasics()
+	{//verify get method i called on mock
+		String value= mock.get(0);
+		String value2 = mock.get(1);
+		// by default verify check for only one time
+		verify(mock).get(0);
+		verify(mock,times(2)).get(anyInt());
+		verify(mock,atLeast(1)).get(anyInt());
+		verify(mock,atMost(2)).get(anyInt());
+		verify(mock,never()).get(2);
 	}
 }
